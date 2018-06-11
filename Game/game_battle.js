f15909dc73dbe68b37f0352e93bc980a255918df
oremez <oremez@starlab.md>
2018-06-11 16:01:30 +0300

    New  (incomplete) version of game

--- Game/game_battle.js
+++ Game/game_battle.js
@@ -2,87 +2,85 @@ console.log('');
 console.log('Консольная битва');
 console.log('');
 
-let myHp = 20, rHp = 20;
-let t = 1;
+let myHp = 20, maniacHp = 20;
+let switching = 1;
 
-const m = ['1. Ударить в голову', '2. Ударить по телу', '3. Ударить по ногам'];
-const n = ['1. Защитить голову', '2. Защитить тело', '3. Защитить ноги'];
-
-let ManiacAction;
-let PlayerAction;
+const attackTactic = ['1. Ударить в голову', '2. Ударить по телу', '3. Ударить по ногам'];
+const protectionTactic = ['1. Защитить голову', '2. Защитить тело', '3. Защитить ноги'];
 
 console.log('На вас напал маньяк. Ваши действия:');
 console.log('');
 
-for (let i = 0; i < m.length; i++) {
-    console.log(m[i]);
-}
+attackTactic.forEach(function(item, i, attackTactic){
+    console.log(attackTactic[i]);
+})
 
-let readline = require('readline');
+const readline = require('readline');
 rl = readline.createInterface(process.stdin, process.stdout);
 
+function AttackActions() {
+    switching *= -1;
+    let blockGeneration = Math.floor(Math.random() * 3 + 1); // генерируем число в диапазоне от 1 до 3 (прибавляем единицу для того, чтобы диапазон не был от 0 до 2)
+    let damage = Math.floor(Math.random() * 5 + 1); // генерируем число в диапазоне от 1 до 5 (прибавляем единицу для того, чтобы диапазон не был от 0 до 4)
+    if (blockGeneration == '3') {
+        console.log('');
+        console.log('Маньяк блокировал удар.');
+        console.log('Его здоровье ' + maniacHp);
+    } else {
+        maniacHp = maniacHp - damage;
+        console.log('');
+        console.log('Вы ударили маньяка.');
+        console.log('Сила удара ' + damage);
+        console.log('Здоровье маньяка ' + maniacHp);
+        if (maniacHp <= 0) {
+            console.log('');
+            console.log('Вы победили.');
+            console.log('Игра оконченна.');
+            process.exit(0);
+        }
+    }
+    protectionTactic.forEach(function(item, i, protectionTactic){
+        console.log(protectionTactic[i]);
+    });
+};
+
+function ProtectActions() {
+    let blockGeneration = Math.floor(Math.random() * 3 + 1); // генерируем число в диапазоне от 1 до 3 (прибавляем единицу для того, чтобы диапазон не был от 0 до 2)
+    let damage = Math.floor(Math.random() * 5 + 1); // генерируем число в диапазоне от 1 до 5 (прибавляем единицу для того, чтобы диапазон не был от 0 до 4)
+    switching *= -1;
+    if (blockGeneration == '3') {
+        console.log('');
+        console.log('Вы заблокировали удар.');
+        console.log('Ваше здоровье ' + myHp);
+    } else {
+        myHp = myHp - damage;
+        console.log('');
+        console.log('Вас ударили.');
+        console.log('Сила удара ' + damage);
+        console.log('Ваше здоровье ' + myHp);
+        if (myHp <= 0) {
+            console.log('');
+            console.log('Вы проиграли.');
+            console.log('Игра оконченна.');
+            process.exit(0);
+        }
+    }
+    attackTactic.forEach(function(item, i, attackTactic){
+        console.log(attackTactic[i]);
+    });
+};
 
 rl.on('line', function(line) {
-
-    let k = line.trim();
-    let z = Math.floor(Math.random() * 3 + 1);
-    let damage = Math.floor(Math.random() * 5 + 1);
-
-    ManiacAction = function() {
-        t *= -1;
-        if (z == k) {
-            console.log('');
-            console.log('Маньяк блокировал удар.');
-            console.log('Его здоровье ' + rHp);
-        } else {
-            rHp = rHp - damage;
-            console.log('');
-            console.log('Вы ударили маньяка.');
-            console.log('Сила удара ' + damage);
-            console.log('Здоровье маньяка ' + rHp);
-            if (rHp <= 0) {
-                console.log('');
-                console.log('Вы победили.');
-                console.log('Игра оконченна.');
-                process.exit(0);
-            }
-        }
-        for (let i = 0; i < n.length; i++) {
-            console.log(n[i]);
-        }
-    };
-
-    PlayerAction = function() {
-        t *= -1;
-        if (z == k) {
-            console.log('');
-            console.log('Вы заблокировали удар.');
-            console.log('Ваше здоровье ' + myHp);
-        } else {
-            myHp = myHp - damage;
-            console.log('');
-            console.log('Вас ударили.');
-            console.log('Сила удара ' + damage);
-            console.log('Ваше здоровье ' + myHp);
-            if (myHp <= 0) {
-                console.log('');
-                console.log('Вы проиграли.');
-                console.log('Игра оконченна.');
-                process.exit(0);
-            }
-        }
-        for (let j = 0; j < m.length; j++) {
-            console.log(m[j]);
-        }
-    };
-
-    if (t > 0) {
-        if (line.trim() == '1' || line.trim() == '2' || line.trim() == '3') {
-            ManiacAction();
+   
+    if (switching > 0) {
+        let selectedNumber = line.trim();
+        if (selectedNumber == '1' || selectedNumber == '2' || selectedNumber == '3') {
+            AttackActions();
         }  
     } else {  
-        if (line.trim() == '1' || line.trim() == '2' || line.trim() == '3') {
-            PlayerAction();
+        let selectedNumber = line.trim();
+        if (selectedNumber == '1' || selectedNumber == '2' || selectedNumber == '3')  {
+            ProtectActions();
         }
     }
 
