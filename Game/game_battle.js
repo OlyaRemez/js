ca50372fd5f17cce65eaa4f484ea9f4329fc9db0
oremez <oremez@starlab.md>
2018-06-11 12:48:40 +0300

    New version of game

--- Game/game_battle.js
+++ Game/game_battle.js
@@ -1,106 +1,89 @@
-console.log("");
-console.log("Консольная битва");
-console.log("");
+console.log('');
+console.log('Консольная битва');
+console.log('');
 
-var myHp = 20, rHp = 20;
-var damage;
-var z;
-var t = 1;
+let myHp = 20, rHp = 20;
+let t = 1;
 
-var m = ["1. Ударить в голову", "2. Ударить по телу", "3. Ударить по ногам"];
-var n = ["1. Защитить голову", "2. Защитить тело", "3. Защитить ноги"];
+const m = ['1. Ударить в голову', '2. Ударить по телу', '3. Ударить по ногам'];
+const n = ['1. Защитить голову', '2. Защитить тело', '3. Защитить ноги'];
 
-var ManiacAction = function(){};
-var PlayerAction = function(){};
+let ManiacAction;
+let PlayerAction;
 
-console.log("На вас напал маньяк. Ваши действия:");
-console.log("");
+console.log('На вас напал маньяк. Ваши действия:');
+console.log('');
 
-for(var i = 0; i < m.length; i++){
+for (let i = 0; i < m.length; i++) {
     console.log(m[i]);
 }
 
-var readline = require('readline');
+let readline = require('readline');
 rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.prompt();
 
 rl.on('line', function(line) {
 
-    if(t > 0){
-
-    z = Math.floor(Math.random() * 3 + 1);
+    let k = line.trim();
+    let z = Math.floor(Math.random() * 3 + 1);
+    let damage = Math.floor(Math.random() * 5 + 1);
 
     ManiacAction = function() {
-        var k = line.trim();
-        if(z == k){
-            console.log("");
-            console.log("Маньяк блокировал удар.");
-            console.log("Его здоровье " + rHp);
-        }
-        else{
-            damage = Math.floor(Math.random() * 5 + 1);
+        t *= -1;
+        if (z == k) {
+            console.log('');
+            console.log('Маньяк блокировал удар.');
+            console.log('Его здоровье ' + rHp);
+        } else {
             rHp = rHp - damage;
-            console.log("");
-            console.log("Вы ударили маньяка.");
-            console.log("Сила удара " + damage);
-            console.log("Здоровье маньяка " + rHp);
-            if(rHp <= 0){
-                console.log("");
-                console.log("Вы победили.");
+            console.log('');
+            console.log('Вы ударили маньяка.');
+            console.log('Сила удара ' + damage);
+            console.log('Здоровье маньяка ' + rHp);
+            if (rHp <= 0) {
+                console.log('');
+                console.log('Вы победили.');
                 console.log('Игра оконченна.');
                 process.exit(0);
             }
         }
-    };
-
-        if(line.trim() == '1' || '2' || '3'){
-            ManiacAction();
-        }  
-  
-
-        for(var i = 0; i < n.length; i++){
+        for (let i = 0; i < n.length; i++) {
             console.log(n[i]);
         }
-        t *= -1;
-    }
-
-    else{
-
-    z = Math.floor(Math.random() * 3 + 1);
+    };
 
     PlayerAction = function() {
-        var k = line.trim();
-        if(z == k){
-            console.log("");
-            console.log("Вы заблокировали удар.");
-            console.log("Ваше здоровье " + myHp);
-        }
-        else{
-            damage = Math.floor(Math.random() * 5 + 1);
+        t *= -1;
+        if (z == k) {
+            console.log('');
+            console.log('Вы заблокировали удар.');
+            console.log('Ваше здоровье ' + myHp);
+        } else {
             myHp = myHp - damage;
-            console.log("");
-            console.log("Вас ударили.");
-            console.log("Сила удара " + damage);
-            console.log("Ваше здоровье " + myHp);
-            if(myHp <= 0){
-                console.log("");
-                console.log("Вы проиграли.");
+            console.log('');
+            console.log('Вас ударили.');
+            console.log('Сила удара ' + damage);
+            console.log('Ваше здоровье ' + myHp);
+            if (myHp <= 0) {
+                console.log('');
+                console.log('Вы проиграли.');
                 console.log('Игра оконченна.');
                 process.exit(0);
             }
         }
-    };
-    
-        if(line.trim() == '1' || '2' || '3'){
-            PlayerAction();
-        }
-    
-        for(var j = 0; j < m.length; j++){
+        for (let j = 0; j < m.length; j++) {
             console.log(m[j]);
         }
-        t *= -1;
+    };
+
+    if (t > 0) {
+        if (line.trim() == '1' || line.trim() == '2' || line.trim() == '3') {
+            ManiacAction();
+        }  
+    } else {  
+        if (line.trim() == '1' || line.trim() == '2' || line.trim() == '3') {
+            PlayerAction();
+        }
     }
 
-rl.prompt();
 });
