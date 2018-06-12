e5f894fe3c91427b2ba4a7302d2bc0fcf1c73e03
oremez <oremez@starlab.md>
2018-06-12 14:54:18 +0300

    New (incomplete???) version of game

--- Game/game_battle.js
+++ Game/game_battle.js
@@ -11,74 +11,86 @@ const protectionTactic = ['1. Защитить голову', '2. Защитит
 console.log('На вас напал маньяк. Ваши действия:');
 console.log('');
 
-attackTactic.forEach(function(item, i, attackTactic){
-    console.log(attackTactic[i]);
+attackTactic.forEach(function(element){
+    console.log(element);
 })
 
 const readline = require('readline');
 rl = readline.createInterface(process.stdin, process.stdout);
 
-function AttackActions() {
+function ifManiacProtect() {
+    console.log('');
+    console.log('Маньяк блокировал удар.');
+    console.log('Его здоровье ' + maniacHp);
+};
+
+function ifManiacDontProtect(){
+    let damage = Math.floor(Math.random() * 5 + 1); // генерируем число в диапазоне от 1 до 5 (прибавляем единицу для того, чтобы диапазон не был от 0 до 4)
+    maniacHp = maniacHp - damage;
+    console.log('');
+    console.log('Вы ударили маньяка.');
+    console.log('Сила удара ' + damage);
+    console.log('Здоровье маньяка ' + maniacHp);
+    if (maniacHp <= 0) {
+        console.log('');
+        console.log('Вы победили.');
+        console.log('Игра оконченна.');
+        process.exit(0);
+    }
+};
+
+function AttackActions(){
     switching *= -1;
     let blockGeneration = Math.floor(Math.random() * 3 + 1); // генерируем число в диапазоне от 1 до 3 (прибавляем единицу для того, чтобы диапазон не был от 0 до 2)
-    let damage = Math.floor(Math.random() * 5 + 1); // генерируем число в диапазоне от 1 до 5 (прибавляем единицу для того, чтобы диапазон не был от 0 до 4)
-    if (blockGeneration == '3') {
-        console.log('');
-        console.log('Маньяк блокировал удар.');
-        console.log('Его здоровье ' + maniacHp);
-    } else {
-        maniacHp = maniacHp - damage;
-        console.log('');
-        console.log('Вы ударили маньяка.');
-        console.log('Сила удара ' + damage);
-        console.log('Здоровье маньяка ' + maniacHp);
-        if (maniacHp <= 0) {
-            console.log('');
-            console.log('Вы победили.');
-            console.log('Игра оконченна.');
-            process.exit(0);
-        }
-    }
-    protectionTactic.forEach(function(item, i, protectionTactic){
-        console.log(protectionTactic[i]);
+
+    ((blockGeneration == '3') ? ifManiacProtect() : ifManiacDontProtect());
+
+    protectionTactic.forEach(function(element){
+        console.log(element);
     });
 };
 
-function ProtectActions() {
-    let blockGeneration = Math.floor(Math.random() * 3 + 1); // генерируем число в диапазоне от 1 до 3 (прибавляем единицу для того, чтобы диапазон не был от 0 до 2)
+function ifPlayerProtect() {
+    console.log('');
+    console.log('Вы заблокировали удар.');
+    console.log('Ваше здоровье ' + myHp);
+};
+
+function ifPlayerDontProtect(){
     let damage = Math.floor(Math.random() * 5 + 1); // генерируем число в диапазоне от 1 до 5 (прибавляем единицу для того, чтобы диапазон не был от 0 до 4)
-    switching *= -1;
-    if (blockGeneration == '3') {
+    myHp = myHp - damage;
+    console.log('');
+    console.log('Вас ударили.');
+    console.log('Сила удара ' + damage);
+    console.log('Ваше здоровье ' + myHp);
+    if (myHp <= 0) {
         console.log('');
-        console.log('Вы заблокировали удар.');
-        console.log('Ваше здоровье ' + myHp);
-    } else {
-        myHp = myHp - damage;
-        console.log('');
-        console.log('Вас ударили.');
-        console.log('Сила удара ' + damage);
-        console.log('Ваше здоровье ' + myHp);
-        if (myHp <= 0) {
-            console.log('');
-            console.log('Вы проиграли.');
-            console.log('Игра оконченна.');
-            process.exit(0);
-        }
+        console.log('Вы проиграли.');
+        console.log('Игра оконченна.');
+        process.exit(0);
     }
-    attackTactic.forEach(function(item, i, attackTactic){
-        console.log(attackTactic[i]);
+};
+
+function ProtectActions() {
+    switching *= -1;
+    let blockGeneration = Math.floor(Math.random() * 3 + 1); // генерируем число в диапазоне от 1 до 3 (прибавляем единицу для того, чтобы диапазон не был от 0 до 2)
+ 
+    ((blockGeneration == '3') ? ifPlayerProtect() : ifPlayerDontProtect());
+
+    attackTactic.forEach(function(element){
+        console.log(element);
     });
 };
 
 rl.on('line', function(line) {
+
+    let selectedNumber = line.trim();
    
     if (switching > 0) {
-        let selectedNumber = line.trim();
         if (selectedNumber == '1' || selectedNumber == '2' || selectedNumber == '3') {
             AttackActions();
         }  
     } else {  
-        let selectedNumber = line.trim();
         if (selectedNumber == '1' || selectedNumber == '2' || selectedNumber == '3')  {
             ProtectActions();
         }
