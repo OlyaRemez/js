d2c3dd2faf134bc0ec1b6b03c7ba86adad90e59e
oremez <oremez@starlab.md>
2018-06-28 14:15:55 +0300

    Kitchen

--- Cafe/Kitchen/Index.ts
+++ Cafe/Kitchen/Index.ts
@@ -1,10 +1,19 @@
-import {Employees} from "./Employee"
-import {Employee} from "./Employee"
-import {Product} from "./Products"
-import {Products} from "./Products"
+import {Employee} from "../Employee/Employee"
+import {Product} from "../Products/Products"
 import {Kitchen} from "./Kitchen"
 
-export const kitEmployee = new Employee('four', 'name', 'chef');
-export const kitchen = new Kitchen(kitEmployee);
+export const kitEmployeeOne = new Employee('four', 'name', 'chef');
+export const kitEmployeeTwo = new Employee('five', 'name', 'assistant chef');
+
+export const first_product = new Product('first_product', 'first_type', 2);
+export const second_product = new Product('second_product', 'second_type', 3);
+export const third_product = new Product('third_product', 'third_type', 5);
+
+export const kitchen = new Kitchen([kitEmployeeOne],[first_product]);
+kitchen.addEmployee(kitEmployeeTwo);
+kitchen.addProduct(second_product);
+kitchen.addProduct(third_product);
+kitchen.printEmployees();
+kitchen.printProducts();
 
 
--- Cafe/Kitchen/Kitchen.ts
+++ Cafe/Kitchen/Kitchen.ts
@@ -1,11 +1,38 @@
-import {Employees} from "./Employee"
-import {Employee} from "./Employee"
-import {Product} from "./Products"
-import {Products} from "./Products"
+import {Employee} from "../Employee/Employee"
+import {Product} from "../Products/Products"
+
 export class Kitchen {
-    private products: Products;
-    private employees: Employees;
-    constructor(kitchenEmployee: Employee){
+    private products: Product[];
+    private employees: Employee[];
+    constructor(employees: Employee[], products: Product[]){
+        this.employees = employees;
+        this.products = products;
+    }
+
+    addEmployee(employee){
+        this.employees.push(employee);
+    }
+
+    addProduct(product){
+        this.products.push(product);
+    }
+
+    printEmployees() {
+        console.log("");
+        console.log("Employees on kitchen: ");
+        this.employees.forEach(function(element){
+          console.log(element.printEmployee());
+        })
+        console.log("");
+    }
+
+    printProducts() {
+        console.log("");
+        console.log("Products: ");
+        this.products.forEach(function(element){
+          console.log(element.printProduct());
+        })
+        console.log("");
     }
 }
 
