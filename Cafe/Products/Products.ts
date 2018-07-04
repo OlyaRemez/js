0409a36cebbe9da6980949aebc9361a94e0f0986
oremez <oremez@starlab.md>
2018-07-04 16:35:28 +0300

    add methods in kitchen

--- Cafe/Products/Products.ts
+++ Cafe/Products/Products.ts
@@ -1,18 +1,26 @@
 export class Product {
-    private name: string;
-    private type: string;
-    private count: number;
-    constructor(name: string, type: string, count: number){
-        this.name = name;
-        this.type = type;
-        this.count = count;
-    }
+  private name: string;
+  private type: string;
+  private count: number;
+  constructor(name: string, type: string, count: number) {
+    this.name = name;
+    this.type = type;
+    this.count = count;
+  }
 
-    printProduct(){
-        console.log(`Product: ${this.name}, ${this.type}, ${this.count}`);
-    }
+  printProduct() {
+    console.log(`Product: ${this.name}, ${this.type}, ${this.count}`);
+  }
 
-    getName(){
-        return this.name;
-    }
+  getName() {
+    return this.name;
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  increaseCount() {
+    this.count = Math.floor(Math.random() * 4 + 1);
+  }
 }
